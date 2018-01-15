af44856ddf045b00e29b1830185441d55d375a34
Dominik Fabisiewicz <domi.fab98@gmail.com>
2018-01-15 10:04:29 +0100

    requests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const bodyParser= require('body-parser');
 var mongodb = require('mongodb');
 
 var url = "mongodb://localhost:27017/citypedia";
@@ -6,7 +7,6 @@ var url = "mongodb://localhost:27017/citypedia";
 const mongoClient = mongodb.MongoClient;
 const app = express();
 let db // Datenbank handle
-
 
 console.log("Creating database...");
 //Connection zur Datenbank
@@ -21,11 +21,48 @@ mongoClient.connect(url, function(err, connection) {
   }
 });
 
-//endpoint cities
-app.get("/api/cities", (req,res) => {
-  res.send(result);
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
 
+
+//
+// GET
+//
+
+app.get("/api/cities", (req,res) => {
+  //werte aus db lesen
+  var werte = ["kek"];
+  res.send(werte);
   res.status(200).end();
 });
+
+
+
+//
+// POST
+//
+app.post("/api/cities", (req,res) => {
+var cityname=req.body.cityname;
+var population=req.body.population;
+var area=req.body.area;
+var avgtemp=req.body.avgtemp;
+var nou=req.body.nou;
+var urate=req.body.rate;
+var isCapital=req.body.isCapital;
+var hasLakes=req.body.hasLakes;
+var hasTrainstation=req.body.hasTrainstation;
+
+console.log("Cityname: " +cityname);
+console.log("Population: "+population);
+console.log("Area: "+area);
+console.log("AvgTemp: " +avgtemp);
+console.log("Nou: " +nou);
+console.log("isCapital: " +isCapital);
+console.log("hasLakes: " +hasLakes);
+console.log("hasTrainstation: " +hasTrainstation);
+res.end("yes");
+});
+
+
 
 app.listen(3000, function (){console.log("Listening on Port 3000!")});
